e66818e58b531ff4ee2d22f60c3b4784c054e702
Diego Guzman Montoya <diego.guzman@grupoeldeber-corp.com>
2026-04-21 09:35:08 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ minHeight: '100vh', display: 'grid', placeItems: 'center' }}>
+      <h1>HOLA  MUNDO</h1>
+    </div>
+  )
+}
+
+export default App
