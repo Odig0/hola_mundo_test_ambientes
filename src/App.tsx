0c2322b3bcb50f50d72a7a1afda15924a088429a
Diego Guzman <diegoguzman366@gmail.com>
2026-04-21 11:38:16 -0400

    add production name

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div style={{ minHeight: '100vh', display: 'grid', placeItems: 'center', background: '#f7f7f7' }}>
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 16 }}>
-        <h1>Hola mundo ambiente desarollo</h1>
+        <h1>Hola mundo ambiente produccion</h1>
         <DinoGame />
       </div>
     </div>
